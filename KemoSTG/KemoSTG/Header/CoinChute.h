36fc6d84b86063f49b94814f72c2290d82220887
Citrine <citrine1105@gmail.com>
2017-05-23 19:15:47 +0900

    コインシュータークラスを追加

---
+++ KemoSTG/KemoSTG/Header/CoinChute.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include "Include.h"
+#include <cmath>
+#define MAX_CREDIT 9U	// 最大クレジット数
+
+class cCoinChute : public iInitialization {
+protected:
+	unsigned int mCoin;	// 投入されたコイン数
+	unsigned int mCredit;	// 所持しているクレジット数
+	int mNeedCoinToCredit;	// 1クレジットに必要なコイン数
+public:
+	cCoinChute();	// デフォルトコンストラクタ
+	cCoinChute(const int NeedCoinToCredit);	// コイン数指定コンストラクタ
+	~cCoinChute();	// デストラクタ
+
+	void SetNeedCoinTo1Credit(const int Coin);	// 1クレジットに必要なコイン数を設定する
+	void UseCredit();	// クレジットを消費する
+	void AddCoin();		// コインを投入する
+	bool GetFreePlayFlag();	// フリープレイ設定か調べる
+	unsigned int GetCoin();	// 現在のコイン数を取得する
+	unsigned int GetCredit();	// 現在のクレジット数を取得する
+	unsigned int GetNeedStartCoin();	// ゲーム開始に必要なコイン数を取得
+	int GetNeedCoinTo1Credit(); // 1クレジットに必要なコイン数を取得する
+
+	void Initialize() override;	// 初期化処理
+	void Finalize() override;	// 終了処理
+};
---
+++ KemoSTG/KemoSTG/Source/CoinChute.cpp
@@ -0,0 +1,89 @@
+﻿#include "../Header/CoinChute.h"
+
+cCoinChute::cCoinChute() {
+	this->Initialize();
+}
+
+cCoinChute::cCoinChute(const int Coin) {
+	this->Initialize();
+	this->SetNeedCoinTo1Credit(Coin);
+}
+
+cCoinChute::~cCoinChute() {
+	this->Finalize();
+}
+
+void cCoinChute::SetNeedCoinTo1Credit(const int Coin) {
+	if (Coin >= 10) {
+		mNeedCoinToCredit = 9;
+	}
+	else if (Coin <= -10) {
+		mNeedCoinToCredit = -9;
+	}
+	else {
+		mNeedCoinToCredit = Coin;
+	}
+}
+
+void cCoinChute::UseCredit() {
+	if (mCredit > 0U) {
+		--mCredit;
+	}
+}
+
+void cCoinChute::AddCoin() {
+	if (!this->GetFreePlayFlag() && mCredit < MAX_CREDIT) {
+		++mCoin;
+		if (mNeedCoinToCredit > 0 && mCoin >= static_cast<unsigned int>(mNeedCoinToCredit)) {
+			++mCredit;
+			mCoin -= mNeedCoinToCredit;
+		}
+		else if (mNeedCoinToCredit < 0) {
+			mCredit += abs(mNeedCoinToCredit);
+			--mCoin;
+		}
+	}
+	if (mCredit > MAX_CREDIT) {
+		mCredit = MAX_CREDIT;
+	}
+}
+
+bool cCoinChute::GetFreePlayFlag() {
+	return mNeedCoinToCredit == 0 ? true : false;
+}
+
+unsigned int cCoinChute::GetCoin() {
+	return mCoin;
+}
+
+unsigned int cCoinChute::GetCredit() {
+	return mCredit;
+}
+
+unsigned int cCoinChute::GetNeedStartCoin() {
+	if (!this->GetFreePlayFlag() && mCredit <= 0U) {
+		if (mNeedCoinToCredit >= 2) {
+			return static_cast<unsigned int>(mNeedCoinToCredit) - mCoin;
+		}
+		else {
+			return 1U;
+		}
+	}
+	return 0U;
+}
+
+int cCoinChute::GetNeedCoinTo1Credit() {
+	return mNeedCoinToCredit;
+}
+
+void cCoinChute::Initialize() {
+	mCoin = 0U;
+	mCredit = 0U;
+	mNeedCoinToCredit = 1;
+}
+
+void cCoinChute::Finalize() {
+	mCoin = 0U;
+	mCredit = 0U;
+	mNeedCoinToCredit = 0;
+}
